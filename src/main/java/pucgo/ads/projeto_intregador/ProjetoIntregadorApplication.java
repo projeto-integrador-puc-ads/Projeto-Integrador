da1003fd3fb4b705751deecf76c3fd8c3df13d0c
caueobm <caue.obm@gmail.com>
2025-09-28 17:26:02 -0300

    alteração de pastas

--- src/main/java/pucgo/ads/projeto_intregador/ProjetoIntregadorApplication.java
+++ src/main/java/pucgo/ads/projeto_intregador/ProjetoIntregadorApplication.java
@@ -1,4 +1,4 @@
-package br.edu.pucgo.ads.projeto_intregador;
+package br.pucgo.ads.projeto_intregador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
