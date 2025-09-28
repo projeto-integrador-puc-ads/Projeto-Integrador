c3ea83cbb8a4e5dffe1c55aaabfbb7fbf7f594ba
caueobm <caue.obm@gmail.com>
2025-09-28 16:54:36 -0300

    First Commit

---
+++ src/main/java/br/edu/pucgo/ads/projeto_intregador/ProjetoIntregadorApplication.java
@@ -0,0 +1,13 @@
+package br.edu.pucgo.ads.projeto_intregador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntregadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntregadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/pucgo/ads/projeto_intregador/ProjetoIntregadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.pucgo.ads.projeto_intregador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntregadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
