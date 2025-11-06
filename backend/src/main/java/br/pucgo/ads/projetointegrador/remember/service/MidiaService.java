a874b78f3df6fe7b8b827f54b28b5e7e7711ee18
Matheus Pires <matheuspiressdev@gmail.com>
2025-11-05 21:34:28 -0300

    Ajuste para rebase

--- backend/src/main/java/br/pucgo/ads/projetointegrador/remember/service/MidiaService.java
+++ backend/src/main/java/br/pucgo/ads/projetointegrador/remember/service/MidiaService.java
@@ -1,9 +1,9 @@
-package br.pucgo.ads.projetointegrador.plataforma.service;
+package br.pucgo.ads.projetointegrador.remember.service;
 
 import br.pucgo.ads.projetointegrador.plataforma.Exception.RecursoNaoEncontradoException;
-import br.pucgo.ads.projetointegrador.plataforma.dto.midia.MidiaResponseDTO;
-import br.pucgo.ads.projetointegrador.plataforma.entity.Midia;
-import br.pucgo.ads.projetointegrador.plataforma.repository.MidiaRepository;
+import br.pucgo.ads.projetointegrador.remember.dto.midia.MidiaResponseDTO;
+import br.pucgo.ads.projetointegrador.remember.entity.Midia;
+import br.pucgo.ads.projetointegrador.remember.repository.MidiaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -30,29 +30,29 @@ public class MidiaService {
      * @param arquivo O arquivo de foto enviado.
      * @return Os dados da mídia salva.
      */
-    public MidiaResponseDTO salvarFoto(MultipartFile arquivo) throws IOException {
-        if (arquivo.isEmpty()) {
-            throw new IllegalArgumentException("Arquivo enviado está vazio.");
-        }
-
-        String nomeArquivo = UUID.randomUUID().toString() + "_" + arquivo.getOriginalFilename();
-        Path caminhoCompleto = Paths.get(uploadPath + nomeArquivo);
-
-        Files.createDirectories(caminhoCompleto.getParent());
-        Files.write(caminhoCompleto, arquivo.getBytes());
-
-        Midia novaMidia = new Midia();
-        novaMidia.setUrlArquivo("/midia/" + nomeArquivo);
-
-        Midia midiaSalva = midiaRepository.save(novaMidia);
-        return new MidiaResponseDTO(midiaSalva);
-    }
+//    public MidiaResponseDTO salvarFoto(MultipartFile arquivo) throws IOException {
+//        if (arquivo.isEmpty()) {
+//            throw new IllegalArgumentException("Arquivo enviado está vazio.");
+//        }
+//
+//        String nomeArquivo = UUID.randomUUID().toString() + "_" + arquivo.getOriginalFilename();
+//        Path caminhoCompleto = Paths.get(uploadPath + nomeArquivo);
+//
+//        Files.createDirectories(caminhoCompleto.getParent());
+//        Files.write(caminhoCompleto, arquivo.getBytes());
+//
+//        Midia novaMidia = new Midia();
+//        novaMidia.setUrlArquivo("/midia/" + nomeArquivo);
+//
+//        Midia midiaSalva = midiaRepository.save(novaMidia);
+//        return new MidiaResponseDTO(midiaSalva);
+//    }
 
     /**
      * Deleta um arquivo de mídia (do servidor e do banco de dados).
      * @param identificador O ID da mídia a ser deletada.
      */
-    public void deletarMidia(UUID identificador) throws IOException {
+    public void deletarMidia(Long identificador) throws IOException {
         Midia midia = midiaRepository.findById(identificador)
                 .orElseThrow(() -> new RecursoNaoEncontradoException("Mídia não encontrada com o ID: " + identificador));
 
