5afa9da8531faf4db0ade7dd8109107ea695ea1c
gfrancop <gfrancop@minsait.com>
2025-11-20 01:17:08 -0300

    config: configuração de CORS (paliativa) e liberação da rota lista-compras no spring secutiry

--- backend/src/main/java/br/pucgo/ads/projetointegrador/plataforma/config/SecurityConfig.java
+++ backend/src/main/java/br/pucgo/ads/projetointegrador/plataforma/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -29,11 +30,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+
             .csrf(csrf -> csrf.disable())
+            .cors(Customizer.withDefaults())
             .authorizeHttpRequests(auth -> auth
                 // Endpoints públicos de autenticação
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers("/api/auth/**").permitAll()
-                
+                .requestMatchers("/lista-compras/**").permitAll()
                 // Endpoints dos outros grupos (protegidos)
                 .requestMatchers("/api/grupo1/**").authenticated()
                 .requestMatchers("/api/grupo2/**").authenticated()
